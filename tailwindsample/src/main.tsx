aae305763a2b248438bec76d14daee37108c0061
kshipradewat <kshipradewat66@gmail.com>
2025-02-08 17:52:34 +0530

    project setup

--- tailwindsample/src/main.tsx
+++ tailwindsample/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.js'
+import App from './App.tsx'
 
-createRoot(document.getElementById('root')).render(
-  <div>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <App />
-  </div>
+  </StrictMode>,
 )
